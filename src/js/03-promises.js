e3146efe58ec31c254ea8ce2e62a8774e2bb57de
Oleg Muraviov <95213060+OlegMuraviov@users.noreply.github.com>
2022-04-06 16:16:31 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,34 @@
 import '../css/03-promises.scss';
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const formNode = document.querySelector('.form');
+
+formNode.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+  console.log(delay.value, step, amount);
+  let delayPromise = Number(delay.value);
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, delayPromise)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayPromise += Number(step.value);
   }
 }
